5e76e78c36ac40400cd64eebb34228430e4cc065
brelee2222 <brelee1842@outlook.com>
2024-06-25 22:24:26 -0700

    Documentation changes

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+/**
+ * The are mainly used to make meownese more compact.
+ * It's holds strings that can be substituted for each other.
+ */
 const accents = {
     "ooooo": "ö",
     "oooo": "ô",
@@ -8,18 +11,21 @@ const accents = {
     "eeee": "ê",
     "eee": "é",
     "ee": "è"
-}
+};
 
-const translateTextbox = document.getElementsByName("translate")[0];
-const meowaccent = document.getElementById("meowaccent");
+const translateTextbox = document.getElementsByName("translate")[0]; // The reference for the big text box :3
+const meowaccent = document.getElementById("meowaccent"); // The reference for the small meow accent checkbox :3
 
+/**
+ * Translates whatever is in the textbox into meownese
+ */
 function clickToMeownese() {
-    translateTextbox.value = translateToMeownese(translateTextbox.value);
-    // console.log(meowaccent.checked);
+    translateTextbox.value = translateToMeownese(translateTextbox.value); // get the text in the textbox and translate to meownese.
+    
     if(meowaccent.checked) {
         let newTextbox = translateTextbox.value;
+
         Object.entries(accents).forEach(entry => {
-            // console.log(entry[0])
             newTextbox = newTextbox.replaceAll(entry[0], entry[1]);
         });
 
@@ -27,14 +33,20 @@ function clickToMeownese() {
     }
 }
 
+/**
+ * Translates whatever is in the textbox back to english (or normal text)
+ */
 function clickToEnglish() {
     let newTextbox = translateTextbox.value;
 
+    // converts accents back to normal. meow accents does not need to be on for this to happen.
     Object.entries(accents).forEach(entry => {
         newTextbox = newTextbox.replaceAll(entry[1], entry[0]);
     });
+
     const translation = translateToEnglish(newTextbox);
 
+    // if you're reading part of the code, know that this code is only here because I wanted to rick roll my friends :3
     if(translation.startsWith("openlink ")) {
         let capitalize = false;
         window.open(translation.replace("openlink ", "").split("").reduce((prev, curr) => {
@@ -56,6 +68,11 @@ function clickToEnglish() {
     translateTextbox.value = translation;
 }
 
+/**
+ * 
+ * @param {*} text normal text to translate to meownese
+ * @returns 
+ */
 function translateToMeownese(text) {
     return detranslate(
         toAlternatingCode(
@@ -66,6 +83,11 @@ function translateToMeownese(text) {
     );
 }
 
+/**
+ * 
+ * @param {*} text meownese text to translate
+ * @returns 
+ */
 function translateToEnglish(text) {
     return decode(
         toMorseCode(
